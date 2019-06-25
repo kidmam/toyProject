9bc8d2089b6d4d8f38a178c753749c0f941e72bb
SangKil Park <ht1911@gmail.com>
2019-06-26 06:32:03 +0900

    Builder pattern separates the construction of a complex object from its representation so that the same construction process can create different representations.

---
+++ designpatterns/Builder/builder.go
@@ -0,0 +1,14 @@
+package main
+
+import "toyProject/designpatterns/Builder/car"
+
+func main() {
+	assembly := car.NewBuilder().Paint(car.RedColor)
+
+	familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
+	familyCar.Drive()
+
+	sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
+	sportsCar.Drive()
+
+}
