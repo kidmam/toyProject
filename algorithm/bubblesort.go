dab9d170e6f370926a0f1752d0668ea5b5324fbb
SangKil Park <ht1911@gmail.com>
2019-07-04 10:17:15 +0900

    Bubble Sort in Go (Golang) with test

---
+++ algorithm/bubblesort.go
@@ -0,0 +1,23 @@
+package algorithm
+
+//https://gochronicles.dev/posts/algorithms/bubble-sort/
+// BubbleSort sorts the slice
+func BubbleSort(nums []int) []int {
+	n := len(nums)
+
+	// Run the outer loop n - 1 times
+	// At the end of each iteration, we have bubbled up ith smallest item
+	for i := 1; i <= n-1; i++ {
+
+		// Run the inner loop on the rest of unsorted slice
+		for j := n - 1; j >= i; j-- {
+
+			// If value at index j is smaller than the one at j-1, swap them
+			if nums[j] < nums[j-1] {
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+			}
+		}
+	}
+
+	return nums
+}
---
+++ algorithm/bubblesort_test.go
@@ -0,0 +1,18 @@
+package algorithm
+
+import (
+	"reflect"
+	"testing"
+)
+
+// go test -v ./...
+func TestBubbleSort(t *testing.T) {
+	nums := []int{23, 1, 53, 543, 98, 4}
+
+	got := BubbleSort(nums)
+	want := []int{1, 4, 23, 53, 98, 543}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v, want: %v, given: %v", got, want, nums)
+	}
+}
