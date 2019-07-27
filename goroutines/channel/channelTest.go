00bb1414298f8c75f88aa673ee58b7c15246cd98
SangKil Park <ht1911@gmail.com>
2019-07-27 17:52:30 +0900

    channel select

---
+++ goroutines/channel/channelTest.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	done := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(done)
+	}()
+
+	workCounter := 0
+loop:
+	for {
+		select {
+		case <-done:
+			break loop
+		default:
+
+		}
+
+		workCounter++
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Printf("Actived %v cycles of work before singnaled to stop.\n", workCounter)
+}
