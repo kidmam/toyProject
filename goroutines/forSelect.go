9e7967adde88b634672ef7da53d776786d8ba0ef
SangKil Park <ht1911@gmail.com>
2019-08-05 05:50:03 +0900

    for~select

--- goroutines/forSelect.go
+++ goroutines/forSelect.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
+	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	//strings := make(chan string)
+	//strings <- "asfasdfasddfasfasdf"
+
+	terminated := doWork(done, nil)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done")
 }
