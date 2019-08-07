e39cc43b6432a88670a38ec21eca49494382cd17
SangKil Park <ht1911@gmail.com>
2019-08-07 16:08:46 +0900

    pipeline

--- goroutines/pipeline.go
+++ goroutines/pipeline.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	genenator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int, len(integers))
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	multiply := func(done <-chan interface{}, intStream <-chan int, multiplier int) <-chan int {
+		multiliedStream := make(chan int)
+		go func() {
+			defer close(multiliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multiliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multiliedStream
+	}
+
+	add := func(done <-chan interface{}, intStream <-chan int, additive int) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := genenator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
+	}
 }
