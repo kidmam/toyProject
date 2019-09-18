6c29c47125fb2c40ee171fc038dc9fc4039def2b
SangKil Park <ht1911@gmail.com>
2019-09-18 19:55:58 +0900

    Prepend

--- datastructures/linkedlist2.go
+++ datastructures/linkedlist2.go
@@ -25,3 +25,18 @@ func (l *LinkedList) Append(n *Node) {
 	l.last = n      // Then, we set the last Node Node to the new Node
 	l.length++      // And finally, we increment the length
 }
+
+func (l *LinkedList) Prepend(n *Node) {
+	// As in Append, first we check if the linked list is empy
+	if l.length == 0 {
+		l.first = n // If it is, we set the first Node the new Node
+		l.last = n  // We also set the last Node to the new Node
+		l.length++  // Finally, we increment the length
+		return
+	}
+
+	firstNode := l.first     // We save our old first Node in a temporal variable
+	l.first = n              // Then, we set the first Node to the new Node
+	l.first.next = firstNode // Finally we linke the new first Node to the old one
+	l.length++               // And then, we increment the length
+}
