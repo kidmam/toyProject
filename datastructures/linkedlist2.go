d881b02fc2b1170d76ac5fa629ec0ece3f6ed63c
SangKil Park <ht1911@gmail.com>
2019-09-21 19:19:22 +0900

    LinkedList

--- datastructures/linkedlist2.go
+++ datastructures/linkedlist2.go
@@ -1,84 +1,75 @@
 package main
 
-// https://dkvist.com/posts/linked-lists-golang/
+import "fmt"
+
 type Node struct {
-	value int   // Our data
-	next  *Node // Pointer (link) to the next Node in the list
+	value int
+	next  *Node
 }
 
 type LinkedList struct {
-	first  *Node // The first Node
-	last   *Node // The last Node
-	length int   // The number of nodes
+	first  *Node
+	last   *Node
+	length int
 }
 
 func (l *LinkedList) Append(n *Node) {
-	// We check if our Linked list is empty
 	if l.length == 0 {
-		l.first = n // If it is, we set the first Node to the new Node
-		l.last = n  // We also set the last Node to the new Node
-		l.length++  // Finally, we increment the length
+		l.first = n
+		l.last = n
+		l.length++
 		return
 	}
 
-	l.last.next = n // We link our last Node to out new last Node
-	l.last = n      // Then, we set the last Node Node to the new Node
-	l.length++      // And finally, we increment the length
+	l.last.next = n
+	l.last = n
+	l.length++
 }
 
 func (l *LinkedList) Prepend(n *Node) {
-	// As in Append, first we check if the linked list is empy
 	if l.length == 0 {
-		l.first = n // If it is, we set the first Node the new Node
-		l.last = n  // We also set the last Node to the new Node
-		l.length++  // Finally, we increment the length
+		l.first = n
+		l.last = n
+		l.length++
 		return
 	}
 
-	firstNode := l.first     // We save our old first Node in a temporal variable
-	l.first = n              // Then, we set the first Node to the new Node
-	l.first.next = firstNode // Finally we linke the new first Node to the old one
-	l.length++               // And then, we increment the length
+	firstNode := l.first
+	l.first = n
+	l.first.next = firstNode
+	l.length++
 }
 
 func (l *LinkedList) Lookup() []int {
-	// If the list is empty we return nil
 	if l.length == 0 {
 		return nil
 	}
 
 	var nodes []int
 	node := l.first
-
-	// If the next Node is nil (doesn't exist) it means that
-	// we're done.
 	for node != nil {
 		nodes = append(nodes, node.value)
 		node = node.next
 	}
 
-	return nodes // Then, we can return the slice with all the values found
+	return nodes
 }
 
 func (l *LinkedList) Delete(v int) {
-	// If the list is empty we have nothing to delete.
 	if l.length == 0 {
 		return
 	}
 
-	// If instead, the Node to delete is the first one on our list
-	// we can remove it directly.
 	if l.first.value == v {
-		l.first = l.first.next // We set the first Node to the second Node
-		l.length--             // Then, we decrement the length of our list
+		l.first = l.first.next
+		l.length--
 		return
 	}
 
-	var prevNode *Node // A reference to the previous Node of the one we are looking for
+	var prevNode *Node
 	node := l.first
+
 	for node.value != v {
-		// If we have reached the end of the list it means that
-		// our Node doesn't exist and that we have nothing to remove.
 		if node == nil {
 			return
 		}
@@ -87,6 +78,37 @@ func (l *LinkedList) Delete(v int) {
 		node = node.next
 	}
 
-	prevNode.next = node.next // We remove the Node by removing the reference to it from the previous one
-	l.length--                // Finally, we decrement the length
+	prevNode.next = node.next
+	l.length--
+}
+
+func main() {
+	l := &LinkedList{}
+
+	secondNode := &Node{
+		value: 2,
+	}
+	l.Append(secondNode)
+
+	thirdNode := &Node{
+		value: 3,
+	}
+	l.Append(thirdNode)
+
+	firstNode := &Node{
+		value: 1,
+	}
+	l.Prepend(firstNode)
+
+	fmt.Println(l.first)
+	fmt.Println(l.first.next)
+	fmt.Println(l.last)
+	fmt.Println(l.length)
+	fmt.Println(l.Lookup())
+
+	l.Delete(1)
+	fmt.Println(l.Lookup())
+
+	l.Delete(3)
+	fmt.Println(l.Lookup())
 }
