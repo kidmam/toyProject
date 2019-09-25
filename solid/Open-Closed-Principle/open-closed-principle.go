b12eadb92ab3a597032f14772d6bc5b76073b2e7
SangKil Park <ht1911@gmail.com>
2019-09-25 18:35:01 +0900

    https://dave.cheney.net/2016/08/20/solid-go-design

---
+++ solid/Open-Closed-Principle/open-closed-principle.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type A struct {
+	year int
+}
+
+func (a A) Greet() { fmt.Println("Hello GolangUK", a.year) }
+
+type B struct {
+	A
+}
+
+func (b B) Greet() { fmt.Println("Welcome to GolangUK", b.year) }
+
+func main() {
+	var a A
+	a.year = 2016
+	var b B
+	b.year = 2016
+	a.Greet() // Hello GolangUK 2016
+	b.Greet() // Welcome to GolangUK 2016
+}
